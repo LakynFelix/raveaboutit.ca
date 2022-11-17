1e7e8f22610f94a90d87d64ea1f035e2150bfa0a
Lakyn <lakynmphotography@hotmail.com>
2022-11-17 16:00:44 -0500

    gallery

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -59,23 +59,26 @@ function showSlides(n) {
 }
 
 // gallery page
-
-$('.portfolio-menu ul li').click(function(){
-    $('.portfolio-menu ul li').removeClass('active');
-    $(this).addClass('active');
-    
-    var selector = $(this).attr('data-filter');
-    $('.portfolio-item').isotope({
-        filter:selector
-    });
-    return  false;
-});
-$(document).ready(function() {
-var popup_btn = $('.popup-btn');
-popup_btn.magnificPopup({
-type : 'image',
-gallery : {
-    enabled : true
-}
-});
-});
+ $('.portfolio-item').isotope({
+         	itemSelector: '.item',
+         	layoutMode: 'fitRows'
+         });
+        $('.portfolio-menu ul li').click(function(){
+            $('.portfolio-menu ul li').removeClass('active');
+            $(this).addClass('active');
+            
+            var selector = $(this).attr('data-filter');
+            $('.portfolio-item').isotope({
+                filter:selector
+            });
+            return  false;
+        });
+        $(document).ready(function() {
+        var popup_btn = $('.popup-btn');
+        popup_btn.magnificPopup({
+        type : 'image',
+        gallery : {
+            enabled : true
+        }
+        });
+        });
